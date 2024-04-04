7a7e4e0e2717edd082b1ebdcc4e26c7792378e11
Nick Giranda <ngiranda@gmail.com>
2024-04-03 22:26:55 -0400

    Added in orbit script for sats.

--- Assets/Orbit.cs
+++ Assets/Orbit.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Orbit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private GameObject target;
 
+    [SerializeField]
+    private Vector3 axis;
+
+    [SerializeField]
+    private float degreePerSecond;
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(target.transform);
+        transform.RotateAround(target.transform.position, axis, degreePerSecond * Time.deltaTime);
     }
 }
